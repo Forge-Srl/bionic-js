358f280a7d7ad3efc0fd004f34fe3019b4eee310
Tommaso Bossi <tommasobossi95@gmail.com>
2020-12-23 14:17:46 +0100

    build(deps): add graceful-fs in /bionic-js-tool
    graceful-fs is a drop-in replacement for the fs module, making various improvements. Specifically fixes an issue regarding the error "EMFILE: too many open files".

--- bionic-js-tool/filesystem/async/fs.js
+++ bionic-js-tool/filesystem/async/fs.js
@@ -1,2 +1,2 @@
 const promisify = require('./promisify')
-module.exports = promisify('fs', 'readFile', 'writeFile', 'readdir', 'stat', 'access', 'unlink')
+module.exports = promisify('graceful-fs', 'readFile', 'writeFile', 'readdir', 'stat', 'access', 'unlink')
